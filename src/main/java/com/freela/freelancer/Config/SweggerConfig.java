e282428d32022f972a793626e6a9706ecaecf105
RafaelKontein23 <rafael.kontein23@gmail.com>
2025-03-06 23:55:06 -0300

    ajuste de erro[

--- src/main/java/com/freela/freelancer/Config/SweggerConfig.java
+++ src/main/java/com/freela/freelancer/Config/SweggerConfig.java
@@ -15,6 +15,8 @@ public class SweggerConfig {
     public OpenAPI openAPI(){
         return new OpenAPI().info(new Info().title("app Frealanccer").description("api para gerenciamento de caditados e vagas").version("1.0.0"))
                 .schemaRequirement("jwt_token",securityScheme());
+
+
     }
     private SecurityScheme securityScheme(){
         return new SecurityScheme().name("jwt_token").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT").in(SecurityScheme.In.HEADER);
