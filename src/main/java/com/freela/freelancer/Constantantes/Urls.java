5e6e30b7edea53573dabb709dc814f1a4837175d
rafael.tein <rafael.tein@visaogrupo.com.br>
2025-03-11 15:42:58 -0300

    ajuste

--- src/main/java/com/freela/freelancer/Constantantes/Urls.java
+++ src/main/java/com/freela/freelancer/Constantantes/Urls.java
@@ -5,4 +5,5 @@ public class Urls {
     public static  String urlBaseIbge = "https://servicodados.ibge.gov.br/api/v1/localidades";
     public static  String urlBaseCep =  "https://viacep.com.br/ws";
     public static String urlbrasilApi = "https://brasilapi.com.br/api/banks/v1";
+
 }
